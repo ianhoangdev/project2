384efbfccb4180887918f325265a4257ff30640c
Ian Hoang <134716435+ianhoangdev@users.noreply.github.com>
2025-10-25 17:42:18 -0400

    added c file

---
+++ MemoryManager.cpp
@@ -0,0 +1,13 @@
+#include "MemoryManager.h"
+#include <unistd.h> 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <utility>
+
+using namespace std;
